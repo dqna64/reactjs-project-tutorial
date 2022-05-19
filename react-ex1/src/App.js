b70a654beaa7ff7d8605f01495e78938373058e6
Gordon Huang <dqna64@gmail.com>
2022-05-20 01:43:45 +1000

    repurpose app for restaurant addressbook project

--- react-ex1/src/App.js
+++ react-ex1/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Restaurants</h1>
+      <p>Here is a list of all the restaurants that I want to keep a record of either because I like them, dislike them, or would like to find out.</p>
+      <div className="card">
+        <h3>Harry's Café de Wheels</h3>
+        <h4>Pies · Hotdogs</h4>
+        <h4>886-896 Princes Hwy, Tempe NSW 2044</h4>
+        <h4>https://harryscafe.loke.app</h4>
+        <p>Has really nice specialty hotdogs and vegan pies. Try the Tiger Pie (mashed peas and potato with gravy)!</p>
+        <div>
+          <div>Not yet visited</div>
+          <div>⭐⭐⭐⭐</div>
+        </div>
+        <div>
+          <button>Visit</button>
+          <div>Rate ⭐⭐⭐⭐⭐</div>
+        </div>
+      </div>
+      <div className="card">
+        <h3>Yasaka Ramen</h3>
+        <h4>Ramen</h4>
+        <h4>644 George St, Sydney NSW 2000</h4>
+        <h4>https://zundoworldsquare.vipguest.net.au/</h4>
+        <p>One of Sydney's best ramen eateries. Located in World Square near lightrail.</p>
+        <div>
+          <div>Visited 2 times</div>
+          <div>⭐⭐⭐⭐</div>
+        </div>
+        <div>
+          <button>Visit</button>
+          <div>Rate ⭐⭐⭐⭐⭐</div>
+        </div>
+      </div>
     </div>
   );
 }
