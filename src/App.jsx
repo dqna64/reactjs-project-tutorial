f15d67acd9d26174c7da8fdad17ef2f5e2757c5b
Gordon Huang <dqna64@gmail.com>
2022-06-28 11:51:20 +1000

    set to template project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
 import './styles/App.css'
-import './styles/Card.css'
-import logo from './assets/logo512.png'
-import { useState } from 'react'
-import Card from './components/Card'
-import placesInfo from './assets/data.json'
 
 const App = () => {
-
-  const [places, setStates] = useState(placesInfo)
-
   return (
-    <>
-      <header>
-        <nav>
-          Food
-          <img src={logo} alt="logo" />
-          Places
-        </nav>
-      </header>
-      <main>
-        <h1>My List</h1>
-        <p>
-          Here is a list of all the restaurants, cafes, dessert bars and other eateries that I want
-          to keep a record of either because I like them, dislike them, or would like to find out.
-        </p>
-        {places.map((place, i) => <Card key={i} info={place}/>)}
-      </main>
-    </>
+    <h1>Hello React</h1>
   )
 }
 
